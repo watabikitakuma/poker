eceb7cd902c6af3158219c74a97ba8420bd3b313
t.watabiki <takuma.watabiki@delight-tech.co.jp>
2018-08-21 17:25:48 +0900

    ポーカーの仮実装。

--- src/main/java/tddbc/poker/Poker.java
+++ src/main/java/tddbc/poker/Poker.java
@@ -2,6 +2,8 @@ package tddbc.poker;
 
 import tddbc.cards.CardCase;
 import tddbc.cards.Cards;
+import tddbc.poker.io.DiscardReader;
+import tddbc.poker.io.Display;
 
 /**
  * ポーカー
@@ -12,10 +14,20 @@ public class Poker {
         Cards cards = new CardCase().getCards();
         Dealer dealer = new Dealer(cards);
         dealer.shuffle();
-        Hand hand = dealer.deal();
-        System.out.println(hand.showCards());
+        Cards dealt = dealer.deal();
+        Hand hand = new Hand(dealt);
+        Display.show(hand.showCards());
+
+        Cards targets = new DiscardReader().read();
+
+        if (targets.isNotEmpty()) {
+            hand.discard(targets);
+            Cards drawCards = dealer.deal(targets.count());
+            hand.draw(drawCards);
+        }
 
         PokerHandsJudge judge = new PokerHandsJudge();
+        System.out.println(hand.showCards());
         System.out.println(judge.call(hand));
     }
 }
